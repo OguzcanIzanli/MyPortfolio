1937a2bbcaa206b29196f316de077c4fd1838e48
oguzcan izanli <oguzcanizanli@gmail.com>
2024-09-04 03:45:52 +0300

    Navbar and logo animations added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
-import "./App.css";
+import "./scss/main.scss";
+import IconStar from "../src/assets/icons/IconStar";
+import About from "./layouts/About";
+import Projects from "./layouts/Projects";
+import Contact from "./layouts/Contact";
 
 function App() {
   const [theme, setTheme] = useState("light");
 
+  const menuItems = [
+    { text: "Home", color: "#60a5fa" },
+    { text: "About", color: "#e879f9" },
+    { text: "Projects", color: "#5eead4" },
+    { text: "Contact", color: "#ffdd1c" },
+  ];
+
   return (
     <>
-      <header className={`${theme == "dark" ? "bgDark" : ""}`}>
-        <div className={`background ${theme}`}></div>
-        {/* <div className="navbar">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Projects</a>
-          <a href="">Contact</a>
-        </div> */}
+      <div className="containerNavbar">
+        <div className="navbar">
+          <div className="logo">
+            <IconStar className="iconStar" />
+          </div>
+          <ul>
+            {menuItems.map((item, index) => (
+              <li
+                key={index}
+                style={{ "--clr": item.color } as React.CSSProperties}
+              >
+                <a href="#" data-text={`${item.text}`}>
+                  {item.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <header className={`${theme === "dark" ? "darkBg" : "lightBg"}`}>
+        <div className={`${theme}`}></div>
+
         <div className="content">
           <h1>An awesome title</h1>
           <div
@@ -24,6 +50,9 @@ function App() {
           </div>
         </div>
       </header>
+      <About />
+      <Projects />
+      <Contact />
     </>
   );
 }
---
+++ src/layouts/About/About.tsx
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div id="about">
+      <div className="T">
+        <div className="top"></div>
+        <div className="middle"></div>
+        <div className="top2"></div>
+        <div className="middle2"></div>
+        <div className="bg"></div>
+        {/* <div className="wrapper">
+          <div className="stem"></div>
+          <div className="top top2"></div>
+        </div>
+        <div className="bg"></div>
+        <div className="bg bg2"></div> */}
+      </div>
+    </div>
+  );
+};
+
+export default About;
