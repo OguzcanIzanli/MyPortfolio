45e4fdaa1055b71239408ceadd654be02554b559
Oğuzcan İzanlı <95178772+OguzcanIzanli@users.noreply.github.com>
2024-09-10 03:53:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import "./scss/main.scss";
 import Navbar from "./components/Navbar";
 import Hero from "./layouts/Hero";
 import About from "./layouts/About";
-import Projects from "./layouts/Projects";
-import Contact from "./layouts/Contact";
 import Footer from "./layouts/Footer";
 
 function App() {
