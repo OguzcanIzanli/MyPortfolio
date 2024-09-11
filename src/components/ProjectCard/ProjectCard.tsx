36d67210591129ae76d2d926a27834f80a127177
oguzcan izanli <oguzcanizanli@gmail.com>
2024-09-11 22:39:04 +0300

    Project section responsive design fixed.

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -4,56 +4,65 @@ import IconWebsite from "../../assets/icons/IconWebsite";
 import Technologies from "../../components/Technologies";
 
 type ProjectCardProps = {
-  viewProjects: boolean;
+  showMore: boolean;
 };
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ viewProjects }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ showMore }) => {
   return (
-    <div
-      className={`card-container ${
-        viewProjects ? "card-showall" : "card-showless"
-      }`}
-    >
-      {projects.map((project) => (
-        <div key={project.name} className="project-card">
-          <div
-            style={{ "--color": project.color } as React.CSSProperties}
-            className="face face-title"
-          >
-            <div className="project-name">{project.name}</div>
-          </div>
-          <div className="face face-up">
+    <div className={`card-container ${showMore ? "show-more" : "show-less"}`}>
+      {projects.map((project, index) => (
+        <div
+          key={project.name}
+          className={`project-card ${index % 2 === 0 ? "left" : "right"}`}
+        >
+          <div className="project-content">
+            {/* Face Title - start */}
             <div
-              style={
-                {
-                  background: project.media,
-                  backgroundSize: "contain",
-                } as React.CSSProperties
-              }
-              className="project-image"
-            ></div>
-          </div>
-          <div
-            style={{ "--color": project.color } as React.CSSProperties}
-            className="face face-down"
-          >
-            <div className="content">
-              <Technologies technologies={project.technologies} />
+              style={{ "--color": project.color } as React.CSSProperties}
+              className="face face-title"
+            >
+              <div className="project-name">{project.name}</div>
+            </div>
+            {/* Face Title - end*/}
 
-              <p>{project.description}</p>
-              <div className="project-links">
-                <a href={project.github} target="_blank">
-                  <span>
-                    <IconGithub /> GitHub
-                  </span>
-                </a>
-                <a href={project.live} target="_blank">
-                  <span>
-                    <IconWebsite /> Live
-                  </span>
-                </a>
+            {/* Face up - start*/}
+            <div className="face face-up">
+              <div
+                style={
+                  {
+                    background: project.media,
+                    backgroundSize: "contain",
+                  } as React.CSSProperties
+                }
+                className="project-image"
+              ></div>
+            </div>
+            {/* Face up - end */}
+
+            {/* Face down - start */}
+            <div
+              style={{ "--color": project.color } as React.CSSProperties}
+              className="face face-down"
+            >
+              <div className="content">
+                <Technologies technologies={project.technologies} />
+                <p>{project.description}</p>
+
+                <div className="project-links">
+                  <a href={project.github} target="_blank">
+                    <span>
+                      <IconGithub /> GitHub
+                    </span>
+                  </a>
+                  <a href={project.live} target="_blank">
+                    <span>
+                      <IconWebsite /> Live
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
+            {/* Face down - end */}
           </div>
         </div>
       ))}
--- src/layouts/Projects/Projects.tsx
+++ src/layouts/Projects/Projects.tsx
@@ -5,18 +5,15 @@ import ProjectCard from "../../components/ProjectCard";
 
 const Projects = () => {
   const { theme } = useTheme();
-  const [viewProjects, setViewProjects] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
   return (
     <div id="projects" className={`${theme ? "light-bg" : "dark-bg"}`}>
       <div className="projects-container">
         <h1 className="projects-title">Projects</h1>
-        <ProjectCard viewProjects={viewProjects} />
-        <button
-          onClick={() => setViewProjects(!viewProjects)}
-          className="view-btn"
-        >
-          <IconChevronsDown className={viewProjects ? "rotate180deg" : ""} />
+        <ProjectCard showMore={showMore} />
+        <button onClick={() => setShowMore(!showMore)} className="view-btn">
+          <IconChevronsDown className={showMore ? "rotate180deg" : ""} />
         </button>
       </div>
     </div>
